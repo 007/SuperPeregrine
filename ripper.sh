bd60885914f952c9448dfc2d6e57338a2692add8
Ryan Moore <ryan@geekportfolio.com>
2019-09-14 23:01:47 -0700

    fix DVD ripping to match titles vs fixed sequence

--- ripper.sh
+++ ripper.sh
@@ -56,11 +56,9 @@ bluray() {
 }
 
 dvd() {
-# probably the same thing here as bluray?
-# need to install a CSS library
-echo "don't know what to do here, CSS library?"
   # extract disc info
-  makemkvcon --cache=1 --minlength=${MIN_LENGTH} --robot info disc:0 > discinfo.txt
+  makemkvcon --cache=100 --minlength=${MIN_LENGTH} --robot info disc:0 > discinfo.txt
+  head -20 discinfo.txt
 
   # extract text metadata
   DISC_LABEL="$(awk -F, '/^CINFO:32,0,/{print $3}' < discinfo.txt | sed 's/"//g')"
@@ -69,21 +67,18 @@ echo "don't know what to do here, CSS library?"
   # total number of tracks to rip
   TITLE_COUNT="$(awk -F: '/^TCOUNT:/{print $2}' < discinfo.txt)"
 
-  # file being encoded for step N-1
-  LAST_FILE="/inbound/last_file"
-
   echo "$(date) Started extracting ${TITLE_COUNT} tracks from ${DISC_LABEL} for ${DISC_DESCRIPTION}"
   echo "$(date) Using ${PRESET_NAME} from ${PRESET_FILE} for encoding"
 
   OUTBOUND_PREFIX="/outbound/${DISC_LABEL}"
   mkdir -p "${OUTBOUND_PREFIX}"
-  # extract raw from disk for track N while re-encoding N-1
-  for TITLE in $(seq 1 $TITLE_COUNT); do
-    MKV_FILE="$(awk -F, "/^TINFO:${TITLE},27/{print \$4}" < discinfo.txt | sed 's/"//g' )"
-    INBOUND_FILE="/inbound/${MKV_FILE}"
-    OUTBOUND_FILE="${OUTBOUND_PREFIX}/${MKV_FILE%.mkv}.mp4"
-    # run this in the background so we can extract next file while it processes
-    HandBrakeCLI --preset-import-file /presets/${PRESET_FILE} --preset ${PRESET_NAME} --input /dev/sr0 --min-duration ${MIN_LENGTH} --title ${TITLE} --optimize --output "${OUTBOUND_FILE}"
+  cp discinfo.txt "${OUTBOUND_PREFIX}"
+
+  # extract by title vs by sequence
+  for TITLE in $(awk -F, '/MSG:3028/{print $8}'< discinfo.txt | sed 's/"//g'); do
+    OUTBOUND_FILE="${OUTBOUND_PREFIX}/title_${TITLE}.mp4"
+    HandBrakeCLI --preset-import-file /presets/${PRESET_FILE} --preset ${PRESET_NAME} --input /dev/sr0 --title ${TITLE} --optimize --output "${OUTBOUND_FILE}"
+    touch "${OUTBOUND_FILE}"
   done
 }
 
@@ -109,7 +104,8 @@ else
 fi
 
 
-chown 1000:1000 ${OUTBOUND_PREFIX}/*
+sync
+chown -R 1000:1001 "${OUTBOUND_PREFIX}"/*
 
 echo "$(date) Finished disc "
 eject /dev/sr0
