c600b72aab109573d37cf89be068e9d3e7bcb78e
Ryan Moore <ryan@geekportfolio.com>
2021-02-10 23:59:23 +0000

    refine x265 params after experimentation

--- helpers/fiver.sh
+++ helpers/fiver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Standard presets for H.265 encoding from high-bitrate source
-time ffmpeg -hide_banner -i "$1" -c:v libx265 -crf 22 -level 4.1 -aq-mode 3 -aq-strength 0.8 -psy-rd 0.8:0:0 -deblock -3:-3 -vf "$(
+time ffmpeg -hide_banner -i "$1" -c:v libx265 -crf 20 -level 4.1 -preset slow -x265-params high-tier=1:repeat-headers=1:aud=1:hrd=1:level-idc=4.1:aq-mode=3:aq-strength=0.8 -vf "$(
 ffmpeg -ss 314 -i "$1" -c:v libx264 -preset ultrafast -t 314 -vf cropdetect -map 0:0 -f null - 2>&1 | awk '/Parsed_cropdetect/{print $14}' | tail -1
-)" -c:a eac3 -c:s srt -b:a 640k -map_metadata -1 -metadata:s:a:0 language=eng -metadata:s:v:0 language=eng -metadata:s:s:0 language=eng -preset slow "${1%.mkv}.265.mkv"
+)" -c:a eac3 -b:a 640k -c:s copy -map_metadata -1 -metadata:s:v:0 language=eng -metadata:s:a:0 language=eng -metadata:s:s:0 language=eng "${2}"
